0dd24dce57596a5cefdfe851858cfbfc43565e25
Md Adnan Baqi <144843601+adnanbaqi@users.noreply.github.com>
2024-02-02 02:50:25 +0530

    Reduced Ip channels , Realtime syncing + custom model support

--- config.py
+++ config.py
@@ -2,30 +2,22 @@ from petals.constants import PUBLIC_INITIAL_PEERS
 
 from data_structures import ModelInfo
 
-INITIAL_PEERS = INITIAL_PEERS = ['/ip4/45.79.153.218/tcp/31337/p2p/QmXfANcrDYnt5LTXKwtBP5nsTMLQdgxJHbK3L1hZdFN8km']
+INITIAL_PEERS = INITIAL_PEERS = ['/ip4/45.79.153.218/tcp/31337/p2p/QmXfANcrDYnt5LTXKwtBP5nsTMLQdgxJHbK3L1hZdFN8km',]
 
 MODELS = [
+
     ModelInfo(
         dht_prefix="StableBeluga2-hf",
         repository="https://huggingface.co/petals-team/StableBeluga2",
         num_blocks=80,
-    ),
-    ModelInfo(
-        dht_prefix="falcon-180B-chat",
-        repository="https://huggingface.co/tiiuae/falcon-180B-chat",
+        ),
+        
+     ModelInfo(
+        dht_prefix="deepseek-coder-33b-instruct",
+        repository="https://huggingface.co/deepseek-ai/deepseek-coder-33b-instruct",
         num_blocks=80,
         limited=True,
-    ),
-    ModelInfo(
-        dht_prefix="Llama-2-70b-chat-hf",
-        repository="https://huggingface.co/meta-llama/Llama-2-70b-chat-hf",
-        num_blocks=80,
-    ),
-    ModelInfo(
-        dht_prefix="Llama-2-70b-hf",
-        repository="https://huggingface.co/meta-llama/Llama-2-70b-hf",
-        num_blocks=80,
-    ),
+        ),
 ]
 
-UPDATE_PERIOD = 60
+UPDATE_PERIOD = 5
